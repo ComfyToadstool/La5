d8981ff5686fe2c5af4ff509211d3e8317d35444
Jennifer N. Smith <jennifersmith@192.168.1.2>
2018-03-16 15:07:53 -0400

    Changed package name. Added classes in rubric. Added header.

---
+++ src/edu/wmich/cs1120/StanRitsema/JenniferSmith/LA5/FileManager.java
@@ -0,0 +1,11 @@
+/* Assignment: CS1120 LA5 Flowers Shop
+ * Author: Stan Ritsema and Jennifer N. Smith
+ * Date: 3/16/18
+ * Reference: LA5_Spring2018.docx (LA5 Instructions)
+ */
+
+package edu.wmich.cs1120.StanRitsema.JenniferSmith.LA5;
+
+public class FileManager {
+
+}
