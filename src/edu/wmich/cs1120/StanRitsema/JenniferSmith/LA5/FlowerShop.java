7a60bc079ba2a86d30a1333f322b64f0d76a2660
Stan <Stan@192.168.0.188>
2018-03-23 20:13:23 -0400

    Retooled shopMenu, created flowerMenu. Most wrong input -> red text

--- src/edu/wmich/cs1120/StanRitsema/JenniferSmith/LA5/FlowerShop.java
+++ src/edu/wmich/cs1120/StanRitsema/JenniferSmith/LA5/FlowerShop.java
@@ -31,9 +31,10 @@ public class FlowerShop {
 	 * options on the menu.
 	 * 
 	 * @param args
+	 * @throws IOException 
 	 */
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		FlowerShop myShop = new FlowerShop();
 		Lookup myLookup = new Lookup();
 		mainMenu(myLookup);
@@ -51,23 +52,77 @@ public class FlowerShop {
 		try {
 
 			System.out.println("Enter your UserName: ");
+			
 			String name = sc.nextLine();
 
 			System.out.println("Enter your Password: ");
+			
 			String pass = sc.nextLine();
 
 			sUser = lookup.checkLoginAuth(name, pass);
 
 		} catch (InvalidInputException e) {
 
-			System.err.flush();
 			System.err.println(e.getMessage());
 			System.err.flush();
 
 		}
 
 	}
+	
+	/**
+	 * Menu controller for adding flowers to the cart.
+	 * Passes true if user bought a flower, false if user
+	 * declined.
+	 * 
+	 * @param lookup
+	 * @return
+	 */
+	public static boolean flowerMenu(Lookup lookup) {
+		
+		try {
+			System.out.println(
+					"Do you want to purchase an item(Y,N)?");
+			
+			String y_n = sc.next().toLowerCase();
 
+			if (y_n.equals("y")) {
+
+				System.out.println("Enter flower id:");
+				
+				String id_pre = sc.next();
+				int id = convert(id_pre);							
+				if (lookup.isValidItem(id)) {
+
+					System.out.println("Enter the quantity:");
+					
+					String quan_pre = sc.next();
+					int quantity = convert(quan_pre);
+
+					Item myItem = lookup.getItemById(id);
+					sUser.addItemToTheLibrary(myItem, quantity);
+
+				} else {
+					throw new InvalidInputException(
+							"This flower's id is not existing!");
+				}
+				return true;
+
+			}else if(y_n.equals("n")){
+				return false;
+			}else {
+				throw new InvalidInputException("Invalid option");
+			}
+
+		} catch (InvalidInputException e) {
+			System.err.println(e.getMessage());
+			System.err.flush();
+		}
+		
+		return true;
+		
+	}
+	
 	/**
 	 * Menu for flower shop
 	 * 
@@ -75,69 +130,37 @@ public class FlowerShop {
 	 *            instance of lookup for the flower shop
 	 * 
 	 */
-
-	public static void shopMenu(Lookup lookup) {
-		while (true) {
+	public static void shopMenu(Lookup lookup) throws InvalidInputException, IOException {
+		
+		int option = -1;
+		
+		while(option != 4) {
+			
 			try {
-				System.out.println("\n1- Flowers List! \n" + "2- My Cart \n"
-						+ "3- Bill \n" + "4- Exit \n"
-						+ "Select one of these options:");
-
-				int option = sc.nextInt();
-				sc.nextLine();
-
-				switch (option) {
-
+				
+				// print menu options
+				System.out.println("\n1- Flowers List! \n"
+				+ "2- My Cart \n" + "3- Bill \n"
+				+ "4- Exit \n" + "Select one of these options: ");
+				
+				
+				String line = sc.next();		
+				option = convert(line);
+				
+				if( (0 >= option) || (5 <= option) ) {
+					throw new InvalidInputException("Invalid option");
+				}
+				
+				switch(option) {
+				
 				case 1:
 					System.out.println(lookup);
+					
+					
+					boolean browse = true;
 
-					while (true) {
-
-						try {
-							System.out.println(
-									"Do you want to purchase an item(Y,N)?");
-							String y_n = sc.nextLine().toLowerCase();
-
-							if (y_n.equals("y")) {
-
-								System.out.println("Enter flower id:");
-								int id = sc.nextInt();
-								sc.nextLine();
-								if (lookup.isValidItem(id)) {
-
-									System.out.println("Enter the quantity:");
-									int quantity = sc.nextInt();
-									sc.nextLine();
-									Item myItem = lookup.getItemById(id);
-									sUser.addItemToTheLibrary(myItem, quantity);
-
-								} else {
-									System.err.flush();
-									System.err.println(
-											"This flower’s id is not existing!");
-									System.err.flush();
-
-								}
-
-							}
-
-							else {
-								break;
-							}
-
-						} catch (InputMismatchException e) {
-
-							System.err.flush();
-							System.err.println(e.getMessage());
-							System.err.flush();
-							sc.nextLine();
-
-						} catch (InvalidInputException e) {
-							System.err.println(e.getMessage());
-							System.err.flush();
-
-						}
-
+					while (browse) {
+						browse = flowerMenu(lookup);
 					}
 					break;
 
@@ -146,66 +169,38 @@ public class FlowerShop {
 					if (list == null) {
 						System.err.println("Your Shopping Cart is empty.");
 						System.err.flush();
-					}
-
-					else {
+					}else {
 						System.out.println(sUser);
 						System.out.println(
 								"Do you want to purchase these items(Y,N)?");
-						String y_n = sc.nextLine().toLowerCase();
+						
+						String y_n = sc.next().toLowerCase();
 						if (y_n.equals("y")) {
-							System.err.flush();
 							sUser.writeBill();
-//							System.err.println("\nYour bill is available now!");
 							System.err.flush();
-
 						}
-
 					}
-
 					break;
 
 				case 3:
-//					String bill = sUser.bill();
-//
-//					if (bill == null) {
-//						System.err
-//								.println("bill was not created.");
-//						System.err.flush();
-//					}
-//
-//					else {
-//						//sUser.writeBill();
-//						// System.out.println(sUser.bill());
-//						sUser.readBill();
-//					}
 					sUser.readBill();
+					System.err.flush();
 					break;
-
-				case 4:
-					System.exit(0);
-
-				default:
-
+				
 				}
-			} catch (IOException e) {
-
+				
+			}catch(InvalidInputException ii) {
+				System.err.println(ii.getMessage());
 				System.err.flush();
-				System.err.println(e.getMessage());
-				System.err.flush();
-
-			} catch (InputMismatchException e) {
-
-				System.err.flush();
-				System.err.println(e.getMessage());
-				System.err.flush();
-				sc.nextLine();
-
 			}
+			
 		}
-
+		
+		System.exit(0);
+		
 	}
 
+	
 	/**
 	 * Sign up a new user
 	 * @param userName Users username
@@ -243,15 +238,17 @@ public class FlowerShop {
 	/**
 	 * Login menu
 	 * @param lookup insance of flower shop lookup 
+	 * @throws IOException 
 	 */
 	
-	public static void mainMenu(Lookup lookup) {
+	public static void mainMenu(Lookup lookup) throws IOException {
 		int option;
 		while (true) {
 
 			System.out.println("Welcome to our Flowers Shop!" + "\n1- SignUp \n"
 					+ "2- Login \n" + "3- Exit\n"
 					+ "Select one of these options:");
+			
 
 			try {
 				option = sc.nextInt();
@@ -265,12 +262,15 @@ public class FlowerShop {
 					while (sUser == null) {
 
 						System.out.println("Enter your UserName: ");
+						
 						userName = sc.nextLine();
 						String password;
 						System.out.println("Enter your Password: ");
+						
 						password = sc.nextLine();
 						String password2;
 						System.out.println("Enter your Password again: ");
+						
 						password2 = sc.nextLine();
 
 						sUser = checkSignUp(userName, password, password2,
@@ -278,7 +278,6 @@ public class FlowerShop {
 
 					}
 
-					System.err.flush();
 					System.err.println("Login Successfully!");
 					System.err.flush();
 					shopMenu(lookup);
@@ -305,7 +304,7 @@ public class FlowerShop {
 			}
 
 			catch (InvalidInputException e) {
-				System.err.flush();
+//				System.err.flush();
 				System.err.print(e.getMessage());
 				System.err.flush();
 
@@ -316,6 +315,17 @@ public class FlowerShop {
 			}
 		}
 
+	}
+	
+	public static int convert(String entry) throws InvalidInputException {
+
+		try {
+			int x = Integer.parseInt(entry);
+			return x;
+		}catch(NumberFormatException nf) {
+			throw new InvalidInputException("The input is not an integer");
+		}
+		
 	}
 
 }
